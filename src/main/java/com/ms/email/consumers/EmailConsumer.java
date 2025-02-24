27920501db0e7e037a4f6753e3afb58e55b48868
ryan <ryantofanini@gmail.com>
2025-02-24 16:13:04 -0300

    consumer and recordDto created

---
+++ src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -0,0 +1,22 @@
+package com.ms.email.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${broker.queue.email.name}" )
+    public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
+    }
+}
